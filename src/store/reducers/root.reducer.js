b2de96286fc7ad6d048fa08d1518c4786124c10c
jkushwaha <jkushwaha@wdtablesystems.com>
2020-08-19 07:20:03 +0530

    added comment

--- src/store/reducers/root.reducer.js
+++ src/store/reducers/root.reducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 import todoReducers from "./todo.reducers";
 
 /**
- * this combined all the reducer and it will be a huge data like this
+ * this combined all the reducer and it will be a huge object(store) like this
  * {
  *     todo:{},
  *     user:{},
