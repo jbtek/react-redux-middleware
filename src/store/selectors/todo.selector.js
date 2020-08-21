bde9129f549b45a23170ab3691ff5d78877316f4
jkushwaha <jkushwaha@wdtablesystems.com>
2020-08-21 15:27:53 +0530

    added redux-thunk,styled-component,persist and lodash

--- src/store/selectors/todo.selector.js
+++ src/store/selectors/todo.selector.js
@@ -1,4 +1,5 @@
 import {createSelector} from 'reselect';
+import {memoize} from 'lodash'
 
 const getTodoState = (state) => state.todo;
 
@@ -6,3 +7,17 @@ export const getTodoList = createSelector(
     getTodoState,
     (todo) => todo.todoList
 )
+
+/**
+ * Now this is the function that get a id param and return the valus, so it will not memoized
+ * because its call as function not as selector
+ * Solution: Need to use lodash method memoize to work like a selector and wrape the
+ * whole function in it. This is the HOC. like this.
+ * @param id
+ */
+export const getDynamicData = (id) => memoize(
+    createSelector(getTodoState,
+        (todo) => todo.todoList.map(item => {
+            return item.id === id
+        }))
+    )
